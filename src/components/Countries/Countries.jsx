652da5ab6ffa856439aa6188955065dbf6a558d3
sowmik-sec <sowmik.sec@gmail.com>
2024-06-24 11:46:45 +0600

    bug fixed: load countries in right file

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+
 function Countries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <div>
-      <h3>Countries</h3>
+      <h3>Countries: {countries.length}</h3>
     </div>
   );
 }
